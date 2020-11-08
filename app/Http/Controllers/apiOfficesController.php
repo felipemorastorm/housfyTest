b8d0be6749f3a7599d8ff9336d3e6a5cb88258dd
Felipe Mora <felipemorastorm@gmail.com>
2020-11-08 23:34:30 +0100

    api status error messagge

---
+++ app/Http/Controllers/apiOfficesController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Filesystem\Cache;
+use Illuminate\Http\Request;
+use App\Models\Offices;
+use Illuminate\Support\Facades\Redis as Redis;
+
+class apiOfficesController extends Controller
+{
+    public function newOffice(Request $request){
+        try {
+            $offices = new Offices();
+            $offices->name = $request->name;
+            $offices->address = $request->address;
+            $offices->save();
+            //all ok return json ok response to api
+            return response()->json([
+                "message" => "goods record created"
+            ], 201);
+        }catch (\Exception $e){
+            return response()->json([
+                "message" => 'Cannot create :'.$e->getMessage()
+            ], 422);
+        }
+    }
+    public function getAllOffices(Request $request){
+        try {
+
+            if(Redis::exists('allOffices')){
+                $offices = (Redis::get('allOffices'));
+            }else {
+                $offices = Offices::all();
+                Redis::set('allOffices',($offices));
+            }
+            return response()->json([
+                "message" => ($offices)
+            ], 201);
+        }catch (\Exception $e){
+            return response()->json([
+                "message" => ('error retrieving data:'.$e->getMessage()
+            ], 422);
+
+
+        }
+    }
+    public function deleteOffice(Request $request){
+        try {
+            $officeToDelete = Offices::find($request->id);
+            if($officeToDelete!=null) {
+                $officeToDelete->delete();
+                //all ok return json ok response to api
+                return response()->json([
+                    "message" => "office with id:".$officeToDelete." deleted sucesfully!"
+                ], 201);
+            }else{
+                return response()->json([
+                    "message" => 'office with id:'.$request->id.' not exist'
+                ], 422);
+            }
+
+        }catch (\Exception $e){
+            return response()->json([
+                "message" => 'Cannot delete :'.$e->getMessage()
+            ], 422);
+        }
+    }
+    public function updateOffice(Request $request){
+        try {
+                $idToUpdate = $request->id;
+                $officeToUpdate = Offices::find($idToUpdate);
+                $officeToUpdate->name = $request->name;
+                $officeToUpdate->address = $request->address;
+                $officeToUpdate->save();
+
+            //all ok return json ok response to api
+            return response()->json([
+                "message" => "office with id:".$idToUpdate." updated sucesfully!"
+            ], 201);
+        }catch (\Exception $e){
+            return response()->json([
+                "message" => 'Cannot update :'.$e->getMessage()
+            ], 422);
+        }
+    }
+
+}
